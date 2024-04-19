7598b1b47a64313d2d6ed7f3c840a978ce270c97
Lerato24321 <123421998+Lerato24321@users.noreply.github.com>
2024-04-20 01:24:35 +0200

    Inserted API for day, humidity and wind speed

--- src/java.js
+++ src/java.js
@@ -5,6 +5,31 @@ function updateWeatherInfo(response) {
 
   let cityName = document.querySelector("#weather-app-city-name");
   cityName.innerHTML = response.data.city;
+
+  let displayHumidity = document.querySelector("#weather-humidity");
+  let humidity = response.data.temperature.humidity;
+  displayHumidity.innerHTML = humidity;
+
+  let displayWind = document.querySelector("#weather-wind");
+  let wind = Math.round(response.data.wind.speed);
+  displayWind.innerHTML = wind;
+
+  let now = new Date();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  let day = days[now.getDay()];
+  let displayDay = document.querySelector("#weather-app-day");
+  displayDay.innerHTML = day;
+
+
 }
 
 function displayCity(city) {
@@ -24,4 +49,4 @@ function searchCity(event) {
 let searchButton = document.querySelector("#search-form");
 searchButton.addEventListener("submit", searchCity);
 
-displayCity ("Vanderbijlpark");
+displayCity("Vanderbijlpark");
