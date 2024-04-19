5ab2dbcc31751e6223abeb6b0a5c5fc6f0f5f982
Lerato24321 <123421998+Lerato24321@users.noreply.github.com>
2024-04-19 20:51:49 +0200

    Added search functionality using API

--- src/java.js
+++ src/java.js
@@ -1,9 +1,27 @@
+function updateWeatherInfo(response) {
+  let currentTemperature = document.querySelector("#main-temperature");
+  let temperature = response.data.temperature.current;
+  currentTemperature.innerHTML = Math.round(temperature);
+
+  let cityName = document.querySelector("#weather-app-city-name");
+  cityName.innerHTML = response.data.city;
+}
+
+function displayCity(city) {
+  let apiKey = "700ad940f9ffadd843o04ae7ba43d1t8";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(updateWeatherInfo);
+}
+
 function searchCity(event) {
   event.preventDefault();
   let cityElement = document.querySelector("#enter-city-name");
-  let cityName = document.querySelector("#weather-app-city-name");
-  cityName.innerHTML = cityElement.value;
+
+  displayCity(cityElement.value);
 }
 
 let searchButton = document.querySelector("#search-form");
 searchButton.addEventListener("submit", searchCity);
+
+displayCity ("Vanderbijlpark");
