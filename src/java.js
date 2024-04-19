795e94f34f6600355f5c99801f966b02540fb947
Lerato24321 <123421998+Lerato24321@users.noreply.github.com>
2024-04-19 19:08:20 +0200

    Added basic search engine without API

--- src/java.js
+++ src/java.js
@@ -1,1 +1,9 @@
+function searchCity(event) {
+  event.preventDefault();
+  let cityElement = document.querySelector("#enter-city-name");
+  let cityName = document.querySelector("#weather-app-city-name");
+  cityName.innerHTML = cityElement.value;
+}
 
+let searchButton = document.querySelector("#search-form");
+searchButton.addEventListener("click", searchCity);
